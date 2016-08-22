afbfd8d2270406ad53fae6082516d3a03298962e
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-08-22 20:54:07 +0100

    Set sessions in state and add /progress end-point

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 'use strict'
-const express    = require('express')
-const session    = require('express-session')
-const fs         = require('fs')
-const cp         = require('child_process')
 const bodyParser = require('body-parser')
+const cp         = require('child_process')
+const express    = require('express')
+const fs         = require('fs')
 const isGitUrl   = require('is-git-url')
+const path       = require('path')
 const redux      = require('redux')
+const session    = require('express-session')
 
-function reducer (state = {sessions:{}}, action) {
+function reducer (state = {}, action) {
     switch(action.type) {
         case 'ADD_SESSION':
-            state.sessions[action.id] = {cloneProgress: 0}
+            state[action.id] = {cloneProgress: 0}
             return state
         case 'UPDATE_CLONE_PROGRESS':
-            state.sessions[action.id].cloneProgress = action.value
+            state[action.id].cloneProgress = action.value
             return state
     }
 }
 
 let store = redux.createStore(reducer)
-
-
-var app = express()
+let app = express()
 
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -40,24 +39,51 @@ app.get('/', function(req, res, next) {
 app.post('/', function(req, res, next) {
     const isGit = isGitUrl(req.body.url)
     if (isGit) {
-        const path = repoToFolder(req.body.url)
-        cp.exec(`git clone --depth=1 ${req.body.url} ${path}`)
+        const path = repoToFolder(req.session.id, req.body.url)
         store.dispatch({type:'ADD_SESSION', id:req.session.id})
+        let pid
+        if (fs.existsSync(path)) {
+            pid = cp.exec('git fetch && git reset --hard origin/HEAD', {cwd:path})
+        }
+        else {
+            pid = cp.exec(`git clone --depth=1 ${req.body.url} ${path}`)
+        }
+        pid.on('exit', (code) => {
+            let action = {type:'UPDATE_CLONE_PROGRESS', id:req.session.id}
+            if (code === 0) {
+                action.value = 100
+            }
+            else {
+                action.value = 'failed'
+            }
+            store.dispatch(action)
+        })
     }
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify(isGit))
+})
+
+app.get('/progress', function(req, res, next) {
+    const state = store.getState()
+    res.setHeader('Content-Type', 'application/json')
+    if (state == null || state[req.session.id] == null)  {
+        return res.end("invalid session")
+    }
+    else {
+        return res.end(JSON.stringify(state[req.session.id].cloneProgress))
+    }
 })
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
 
-function repoToFolder(repoURL)  {
-    var folder = repoURL.replace(/^http:\/\//,'')
+function repoToFolder(id, repoURL)  {
+    let folder = repoURL.replace(/^http:\/\//,'')
     folder = folder.replace(/^https:\/\//,'')
     folder = folder.replace(/^git:\/\//,'')
     folder = folder.replace(/^.+?@/,'')
     folder = folder.replace(/:/,'/')
-    return `tmp/${folder}`
+    return path.join('tmp', id, folder)
 }
 
