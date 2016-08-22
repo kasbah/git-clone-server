4f4a058043b6e4a86494784c6da2e6b79e366a02
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-08-22 19:55:32 +0100

    Work on new approach to clone, use redux

--- app.js
+++ app.js
@@ -1,58 +1,63 @@
 'use strict'
-var express = require('express')
-var session = require('express-session')
-var git     = require('nodegit')
-var fs      = require('fs')
+const express    = require('express')
+const session    = require('express-session')
+const fs         = require('fs')
+const cp         = require('child_process')
+const bodyParser = require('body-parser')
+const isGitUrl   = require('is-git-url')
+const redux      = require('redux')
+
+function reducer (state = {sessions:{}}, action) {
+    switch(action.type) {
+        case 'ADD_SESSION':
+            state.sessions[action.id] = {cloneProgress: 0}
+            return state
+        case 'UPDATE_CLONE_PROGRESS':
+            state.sessions[action.id].cloneProgress = action.value
+            return state
+    }
+}
+
+let store = redux.createStore(reducer)
+
 
 var app = express()
 
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function(req, res, next) {
-    var sess = req.session
-    const path = repoToFolder(req.query.url)
-    if (fs.existsSync(path)) {
-        //git fetch origin && git reset --hard origin/master
-        git.Repository.open(path).then((repo) => {
-            repo.getCurrentBranch().then((branch) => {
-                repo.fetch('origin').then(() => {
-                    git.Branch.upstream(branch).then((upstream) => {
-                        const p = git.Reset.reset(repo, upstream, git.Reset.TYPE.HARD)
-                        console.log(p)
-                        p.catch((err) => console.log('error', error))
-                        p.then((success) => console.log('ret', success))
-                    })
-                })
-            })
-        })
+    res.setHeader('Content-Type', 'text/html')
+    res.write(
+        `<form action="/" method="post">
+              <input type="text" name="url"
+                    value="https://github.com/kasbah/nomech"><br>
+              <input type=submit value=Submit>
+        </form>`)
+    res.end()
+})
+
+app.post('/', function(req, res, next) {
+    const isGit = isGitUrl(req.body.url)
+    if (isGit) {
+        const path = repoToFolder(req.body.url)
+        cp.exec(`git clone --depth=1 ${req.body.url} ${path}`)
+        store.dispatch({type:'ADD_SESSION', id:req.session.id})
     }
-    else {
-        git.Clone(req.query.url, path).catch((err) => {
-            console.error(err)
-        }).then((repo) => {
-            console.log(repo)
-        })
-    }
-    if (sess.url) {
-      res.setHeader('Content-Type', 'text/html')
-      res.write('<p>url: ' + sess.url + '</p>')
-      res.write('<p>expires in: ' + (sess.cookie.maxAge / 1000) + 's</p>')
-      res.end()
-    } else {
-      sess.url = req.
-      res.end('welcome to the session demo. refresh!')
-    }
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(isGit))
 })
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
+    console.log('Example app listening on port 3000!')
+})
 
-function repoToFolder(repo)  {
-    var folder = repo.replace(/^http:\/\//,'');
-    folder = folder.replace(/^https:\/\//,'');
-    folder = folder.replace(/^git:\/\//,'');
-    folder = folder.replace(/^.+?@/,'');
-    folder = folder.replace(/:/,'/');
-    return `/tmp/${folder}`
+function repoToFolder(repoURL)  {
+    var folder = repoURL.replace(/^http:\/\//,'')
+    folder = folder.replace(/^https:\/\//,'')
+    folder = folder.replace(/^git:\/\//,'')
+    folder = folder.replace(/^.+?@/,'')
+    folder = folder.replace(/:/,'/')
+    return `tmp/${folder}`
 }
+
