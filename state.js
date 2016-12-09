29bae41d1eac7edff53aa5fdd162589deb305d53
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-09 21:10:48 +0000

    Sketch out state a bit

--- state.js
+++ state.js
@@ -1,5 +1,31 @@
+//@flow
 const Immutable = require('immutable')
+const redux = require('redux')
 
 const initial_state = Immutable.Map({
     sessions: Immutable.Map()
 })
+
+function reducer(state = initial_state, action) {
+    let sessions = state.get('sessions')
+    let session = sessions.get(action.session_id)
+    if (session == null) {
+        session = Immutable.Map({
+            repos: Immutable.List()
+        })
+    }
+    session = reduceSession(session, action)
+    sessions = sessions.set(action.session_id, session)
+    return state.set('sessions', sessions)
+}
+
+
+function reduceSession(session, action) {
+    switch(action.type) {
+        case 'PREVIEW_LINK':
+            return
+    }
+    return session
+}
+
+module.exports = redux.createStore(reducer, initial_state)
