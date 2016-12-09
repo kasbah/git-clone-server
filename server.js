793d8cd0f3c17c7a0de8186b4e187051d0012d8b
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-09 14:56:15 +0000

    Figure out class return for graphql

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var express         = require('express')
+var graphqlHTTP     = require('express-graphql')
+var { buildSchema } = require('graphql')
+const isGitUrl      = require('is-git-url')
 
-// Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    repo(url : String): Int
+    repo(url : String): Repo | Error
   }
-`);
 
-// The root provides a resolver function for each API endpoint
+  type Repo {
+    progress: Int
+    folder: String
+  }
+`)
+
+class Repo {
+    constructor(url) {
+        this.folder = repoToFolder(url)
+        this.progress = 0
+    }
+}
+
 var root = {
-  repo: () => {
-    return 0
-  },
-};
+    repo: ({url}) => {
+        return new Repo(url)
+    },
+}
 
-var app = express();
+var app = express()
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
-app.listen(4000);
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}))
+app.listen(4000)
+console.log('Running a GraphQL API server at localhost:4000/graphql')
+
+function repoToFolder(repoURL)  {
+    let folder = repoURL.replace(/^http:\/\//,'')
+    folder = folder.replace(/^https:\/\//,'')
+    folder = folder.replace(/^git:\/\//,'')
+    folder = folder.replace(/^.+?@/,'')
+    return folder.replace(/:/,'/')
+}
