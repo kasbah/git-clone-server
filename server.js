00bde9d1aef66dcb212673384ad58489ba4f41e6
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-09 19:16:43 +0000

    Start with simple cookie session

--- server.js
+++ server.js
@@ -3,45 +3,47 @@ const graphqlHTTP     = require('express-graphql')
 const { buildSchema } = require('graphql')
 const isGitUrl        = require('is-git-url')
 const graphqlTools    = require('graphql-tools')
+const cookieSession   = require('cookie-session')
+const shortid         = require('shortid')
 
 const schema = `
-  type UserError {
-    message : String
-  }
+   type UserError {
+       message : String
+   }
 
-  type Repo {
-    progress : Int
-    folder   : String
-  }
+    type Repo {
+        progress : Int
+        folder   : String
+    }
 
-  union Result = Repo | UserError
+    union Result = Repo | UserError
 
-  type Query {
-    repo(url : String) : Result
-  }
+    type Query {
+        repo(url : String) : Result
+    }
 
-  schema {
-    query : Query
-  }
+    schema {
+        query : Query
+    }
 `
 
 const resolverMap = {
    Query: {
-      repo(_, {url}) {
-         if (! isGitUrl(url)) {
-            return {message: 'Invalid git URL'}
-         }
-         return {folder: repoToFolder(url), progress: 0}
-      }
+       repo(_, {url}) {
+           if (! isGitUrl(url)) {
+               return {message: 'Invalid git URL'}
+           }
+           return {folder: repoToFolder(url), progress: 0}
+       }
    },
    Result: {
       __resolveType(root, context, info){
-         if (root.folder != null) {
-            return 'Repo'
-         } else if (root.message != null) {
-            return 'UserError'
-         }
-         return null;
+          if (root.folder != null) {
+              return 'Repo'
+          } else if (root.message != null) {
+              return 'UserError'
+          }
+          return null;
       },
    },
 }
@@ -56,8 +58,22 @@ app.use('/graphql', graphqlHTTP({
     schema: executableSchema,
     graphiql: true,
 }))
+app.use(cookieSession({
+    name: 'session',
+    keys: ['keep.emsu.pers.ecret'],
+
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 app.listen(4000)
 console.log('Running a GraphQL API server at localhost:4000/graphql')
+
+app.get('/*', function(req, res, next) {
+    if (! req.session.id) {
+        req.session.id = shortid.generate()
+    }
+    return res.send('hey')
+})
 
 function repoToFolder(repoURL)  {
     let folder = repoURL.replace(/^http:\/\//,'')
---
+++ state.js
@@ -0,0 +1,5 @@
+const Immutable = require('immutable')
+
+const initial_state = Immutable.Map({
+    sessions: Immutable.Map()
+})
