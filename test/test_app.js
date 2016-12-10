6f57ff22507c217f96ec5837e18b2503a7235e3b
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-10 21:11:46 +0000

    Add session persistence test

--- test/test_app.js
+++ test/test_app.js
@@ -8,7 +8,7 @@ const createExpressWrapper = require('graphql-tester/lib/main/servers/express.js
 
 const app = require('../lib/app')
 
-describe('api' , () => {
+describe('API' , () => {
   const test = graphqlTester.tester({
     server: createExpressWrapper(app),
     url: '/graphql'
@@ -29,6 +29,18 @@ describe('api' , () => {
         expect(response2.success).to.be.ok
         expect(response2.status).to.equal(200)
         expect(response2.data.sessionId).to.not.equal(response1.data.sessionId)
+        return done()
+      })
+    })
+  })
+  it('persists session ids through cookies', done => {
+    test('{sessionId}', {jar: true}).then(response1 => {
+      expect(response1.success).to.be.ok
+      expect(response1.status).to.equal(200)
+      test('{sessionId}', {jar: true}).then(response2 => {
+        expect(response2.success).to.be.ok
+        expect(response2.status).to.equal(200)
+        expect(response2.data.sessionId).to.equal(response1.data.sessionId)
         return done()
       })
     })
