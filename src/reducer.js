e137a7ebba8347eab712e3dd18b187fe1a3fb644
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-10 01:04:05 +0000

    Split out reducers from actions

---
+++ src/reducer.js
@@ -0,0 +1,56 @@
+//@flow
+const Immutable = require('immutable')
+const redux = require('redux')
+
+type State = {
+    sessions : Immutable.Map<String, Session>
+}
+
+const initial_state : State = {
+    sessions: Immutable.Map()
+}
+
+type Action = {
+    type: String,
+    session_id: String,
+    value: String
+}
+
+
+type Session = {
+    repos: Immutable.Map<String, RepoStatus>
+}
+
+type RepoStatus = 'start' | 'in_progress' | 'done'
+
+const reducers = {
+    startClone(session, value) {
+        const repo = session.repos.get(value)
+        if (repo == null || repo == 'done') {
+            const repos = session.repos.set(value, 'start')
+            return Object.assign(session, {repos})
+        }
+        return session
+    }
+}
+
+
+function reducer(state: State, action: Action): State {
+    if (state == null) {
+        state = initial_state
+    }
+    let session : ?Session = state.sessions.get(action.session_id)
+    if (session == null) {
+        session = {
+            repos: Immutable.Map()
+        }
+    }
+    session = Object.keys(reducers).reduce((session, name) => {
+        return reducers[name](session, action.value)
+    }, session)
+    const sessions = state.sessions.set(action.session_id, session)
+    return {sessions}
+}
+
+
+module.exports = {reducers, reducer, initial_state}
--- test/test.js
+++ test/test.js
@@ -1,26 +1,33 @@
 const {expect} = require('chai')
-const actions = require('../lib/actions')
+const {reducer, initial_state} = require('../lib/reducer')
 
 describe('reducer', () => {
     it('should add a new session', (done) => {
-        const state = actions.reducer(actions.initial, {type: 'startClone', session_id: 'id', value: 'value'})
+        const state = reducer(initial_state, {type: 'startClone', session_id: 'id', value: 'value'})
         expect(state.sessions.size).to.equal(1)
         return done()
     })
     it('should not add session twice', (done) => {
-        const state1 = actions.reducer(actions.initial, {type: 'startClone', session_id: 'id', value: 'value'})
+        const state1 = reducer(initial_state, {type: 'startClone', session_id: 'id', value: 'value'})
         expect(state1.sessions.size).to.equal(1)
-        const state2 = actions.reducer(state1, {type: 'startClone', session_id: 'id', value: 'value'})
+        const state2 = reducer(state1, {type: 'startClone', session_id: 'id', value: 'value'})
         expect(state1.sessions.size).to.equal(1)
         expect(state2.sessions.size).to.equal(1)
         return done()
     })
     it('should add another session', (done) => {
-        const state1 = actions.reducer(actions.initial, {type: 'startClone', session_id: 'id1', value: 'value'})
+        const state1 = reducer(initial_state, {type: 'startClone', session_id: 'id1', value: 'value'})
         expect(state1.sessions.size).to.equal(1)
-        const state2 = actions.reducer(state1, {type: 'startClone', session_id: 'id2', value: 'value'})
+        const state2 = reducer(state1, {type: 'startClone', session_id: 'id2', value: 'value'})
         expect(state1.sessions.size).to.equal(1)
         expect(state2.sessions.size).to.equal(2)
+        return done()
+    })
+    it('should add a repo with start state', (done) => {
+        const session_id = 'id'
+        const value = 'value'
+        const state = reducer(initial_state, {type: 'startClone', session_id, value})
+        expect(state.sessions.get(session_id).repos.get(value)).to.equal('start')
         return done()
     })
 })
