7ecb284cb963bc188de9fe13891deddf4c120f8b
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-10 01:58:42 +0000

    Test actions

--- src/actions.js
+++ src/actions.js
@@ -4,10 +4,16 @@ const {mainReducer, reducers} = require('./reducers')
 
 const store = redux.createStore(mainReducer)
 
-const actions = {}
-Object.keys(reducers).forEach(name => {
-    actions[name] = function actionDispatch(session_id, value) {
-        return store.dispatch({type: name, session_id, value})
-    }
-})
-module.exports = {actions, store}
+function makeActions(store) {
+    const actions = {}
+    Object.keys(reducers).forEach(name => {
+        actions[name] = function actionDispatch(session_id, value) {
+            return store.dispatch({type: name, session_id, value})
+        }
+    })
+    return actions
+}
+
+const actions = makeActions(store)
+
+module.exports = {makeActions, actions, store}
---
+++ test/test_actions.js
@@ -0,0 +1,32 @@
+const {expect} = require('chai')
+const redux = require('redux')
+
+const {mainReducer, reducers} = require('../lib/reducers')
+const {makeActions} = require('../lib/actions')
+
+describe('actions', () => {
+    it('should not mutate state', done => {
+        const store      = redux.createStore(mainReducer)
+        const actions    = makeActions(store)
+        const session_id = 'id'
+        const value      = 'value'
+        const state1     = store.getState()
+        expect(state1.get('sessions').size).to.equal(0)
+        actions.startClone(session_id, value)
+        expect(state1.get('sessions').size).to.equal(0)
+        done()
+    })
+    it('should dispatch', done => {
+        const store      = redux.createStore(mainReducer)
+        const actions    = makeActions(store)
+        const session_id = 'id'
+        const value      = 'value'
+        const state1     = store.getState()
+        expect(state1.get('sessions').size).to.equal(0)
+        actions.startClone(session_id, value)
+        const state2 = store.getState()
+        expect(state1.get('sessions').size).to.equal(0)
+        expect(state2.get('sessions').size).to.equal(1)
+        done()
+    })
+})
