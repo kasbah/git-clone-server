1fe796a48f445e3d65561c7b9a4865750c649627
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-11 23:33:03 +0000

    Play around with client some more

--- client/client.js
+++ client/client.js
@@ -5,13 +5,30 @@ var input = document.querySelector('input')
 
 form.onsubmit = function submitUrl(event) {
     event.preventDefault()
+    console.log(input.value)
     var xhr = new XMLHttpRequest()
     xhr.responseType = 'json'
     xhr.open("POST", "/graphql")
     xhr.setRequestHeader("Content-Type", "application/json")
     xhr.setRequestHeader("Accept", "application/json")
     xhr.onload = function () {
-        console.log('data returned:', xhr.response)
+        if (xhr.response.data.addRepo.message != null) {
+            console.log(xhr.response.data.addRepo.message)
+        } else {
+            console.log(xhr.response.data.addRepo)
+        }
     }
-    xhr.send(JSON.stringify({query: "{ sessionId }"}))
+    var query = `
+        mutation ($url: String!) {
+          addRepo(url: $url) {
+            ... on Repo {
+              status
+            }
+            ... on UserError {
+              message
+            }
+          }
+        }
+    `
+    xhr.send(JSON.stringify({query: query, variables: {url:input.value}}))
 }
