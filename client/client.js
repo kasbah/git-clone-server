3a987efaf598e7fe7ef2e34e5d0a161bc3da912f
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-11 21:20:55 +0000

    Play around with client

--- client/client.js
+++ client/client.js
@@ -1,10 +1,17 @@
 console.log('hey')
-var xhr = new XMLHttpRequest()
-xhr.responseType = 'json'
-xhr.open("POST", "/graphql")
-xhr.setRequestHeader("Content-Type", "application/json")
-xhr.setRequestHeader("Accept", "application/json")
-xhr.onload = function () {
-  console.log('data returned:', xhr.response)
+
+var form = document.querySelector('form')
+var input = document.querySelector('input')
+
+form.onsubmit = function submitUrl(event) {
+    event.preventDefault()
+    var xhr = new XMLHttpRequest()
+    xhr.responseType = 'json'
+    xhr.open("POST", "/graphql")
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.setRequestHeader("Accept", "application/json")
+    xhr.onload = function () {
+        console.log('data returned:', xhr.response)
+    }
+    xhr.send(JSON.stringify({query: "{ sessionId }"}))
 }
-xhr.send(JSON.stringify({query: "{ sessionId }"}))
